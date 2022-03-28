ae4c4d27fface650dac6a80f9015ace72bff84d4
ComputerGeek5 <drin.karkini@gmail.com>
2022-03-28 22:49:17 +0200

    Worked on back and added login and signup

--- Back/src/main/java/com/example/spamclassifier/controller/InboxController.java
+++ Back/src/main/java/com/example/spamclassifier/controller/InboxController.java
@@ -1,12 +1,38 @@
-package com.example.spamclassifier.controller;
+package com.example.spamclassifier.controller.api;
 
+import com.example.spamclassifier.controller.api.response.BodyResponse;
+import com.example.spamclassifier.service.abst.MailService;
+import com.example.spamclassifier.service.abst.UserService;
 import com.example.spamclassifier.util.annotation.BaseURL;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @BaseURL
 @RestController
 public class InboxController {
 
+    private final UserService userService;
+    private final MailService mailService;
 
+    @Autowired
+    public InboxController(UserService userService, MailService mailService) {
+        this.userService = userService;
+        this.mailService = mailService;
+    }
+
+    @GetMapping("/inbox")
+    @ResponseBody
+    public BodyResponse getReceivedMails() {
+        return null;
+    }
+
+
+    @PostMapping("/compose")
+    @ResponseBody
+    public BodyResponse compose() {
+        return null;
+    }
 }
